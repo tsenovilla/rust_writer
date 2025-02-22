baf6edba7518c3266af580be5eb71199909f24ad
Tomás Senovilla Polo <tspscgs@gmail.com>
2025-02-22 19:41:51 +0100

    fmt

--- rust_writer_procedural/tests/compile_fail.rs
+++ rust_writer_procedural/tests/compile_fail.rs
@@ -3,7 +3,7 @@
 use trybuild::TestCases;
 
 #[test]
-fn compile_fail(){
-    let t = TestCases::new();
-    t.compile_fail("tests/ui/*.rs");
+fn compile_fail() {
+	let t = TestCases::new();
+	t.compile_fail("tests/ui/*.rs");
 }
