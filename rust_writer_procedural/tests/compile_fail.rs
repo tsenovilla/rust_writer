ca0b2868b741fd4b9c57db250eb0d28261f9b7b9
Tomás Senovilla Polo <tspscgs@gmail.com>
2025-02-22 19:39:31 +0100

    First trybuild test

---
+++ rust_writer_procedural/tests/compile_fail.rs
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+
+use trybuild::TestCases;
+
+#[test]
+fn compile_fail(){
+    let t = TestCases::new();
+    t.compile_fail("tests/ui/*.rs");
+}
---
+++ rust_writer_procedural/tests/ui/mutator_fails_if_no_implementor.rs
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0
+
+use rust_writer_procedural::mutator;
+
+#[mutator]
+struct SomeStruct;
+
+fn main(){}
