85e3816a50e805acfff6cfd0c3b68b80f93c3aed
Tomás Senovilla Polo <tspscgs@gmail.com>
2025-02-26 20:02:01 +0100

    modified struct with generics fields test

--- rust_writer_procedural/tests/modified_struct_with_generics.rs
+++ rust_writer_procedural/tests/modified_struct_with_generics.rs
@@ -13,8 +13,8 @@ use test_builder::TestBuilder;
 #[mutator(ItemToTrait<'a>, ItemToImpl<'a>)]
 #[finder(ItemToImpl<'a>, ItemToTrait<'a>)]
 #[impl_from]
-struct SomeStruct<T: Clone + std::fmt::Debug + From<String>>{
-    some_super_useful_field: T
+struct SomeStruct<T: Clone + std::fmt::Debug + From<String>> {
+	some_super_useful_field: T,
 }
 
 #[test]
@@ -34,20 +34,22 @@ fn modified_struct_with_generics() {
 		)
 			.into();
 
-		let some_struct: SomeStruct<Vec<u8>> = (vec![1,2,3],item_to_trait, item_to_impl ).into();
+		let some_struct: SomeStruct<Vec<u8>> = (vec![1, 2, 3], item_to_trait, item_to_impl).into();
 
 		let ast = builder.get_mut_ast_file("trait_and_impl_block.rs").expect("This should exist");
 
-		let mut finder: SomeStructFinderWrapper<Vec<u8>> = Finder::default().to_find(&some_struct).into();
+		let mut finder: SomeStructFinderWrapper<Vec<u8>> =
+			Finder::default().to_find(&some_struct).into();
 		assert!(!finder.find(ast));
 
 		let mut mutator: SomeStructMutatorWrapper<Vec<u8>> =
 			Mutator::default().to_mutate(&some_struct).into();
 		assert!(mutator.mutate(ast).is_ok());
 
-		let mut finder: SomeStructFinderWrapper<Vec<u8>> = Finder::default().to_find(&some_struct).into();
+		let mut finder: SomeStructFinderWrapper<Vec<u8>> =
+			Finder::default().to_find(&some_struct).into();
 		assert!(finder.find(ast));
 
-    assert_eq!(some_struct.some_super_useful_field, vec![1,2,3]);
+		assert_eq!(some_struct.some_super_useful_field, vec![1, 2, 3]);
 	});
 }
