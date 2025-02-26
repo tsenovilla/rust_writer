54d4f2b64ef42fd446f66bfee54eda57a3d00dd1
Tomás Senovilla Polo <tspscgs@gmail.com>
2025-02-26 18:39:08 +0100

    different implementors set if not impl from test

--- rust_writer_procedural/tests/impl_from_with_outermost_macro_implementors_set_bigger_than_innermost_macro_implementors_set.rs
+++ rust_writer_procedural/tests/impl_from_with_outermost_macro_implementors_set_bigger_than_innermost_macro_implementors_set.rs
@@ -1,22 +1,22 @@
 // SPDX-License-Identifier: GPL-3.0
 
 use rust_writer::ast::{
-	finder::{ToFind},
+	finder::ToFind,
 	implementors::{ItemToImpl, ItemToTrait},
-	mutator::{ToMutate},
+	mutator::ToMutate,
 };
 
-use rust_writer_procedural::{finder, mutator, impl_finder};
+use rust_writer_procedural::{finder, impl_finder, mutator};
 use syn::{parse_quote, visit::Visit, visit_mut::VisitMut, ImplItem, TraitItem};
 use test_builder::TestBuilder;
 
 #[impl_finder]
 #[derive(Debug, Clone)]
-struct ToyFinderImplementor{
-    found: [bool; 0]
+struct ToyFinderImplementor {
+	found: [bool; 0],
 }
 
-impl Visit<'_> for ToyFinderImplementor{}
+impl Visit<'_> for ToyFinderImplementor {}
 
 #[finder(ItemToImpl<'a>, ItemToTrait<'a>, local = ToyFinderImplementor)]
 #[mutator(ItemToTrait<'a>, ItemToImpl<'a>)]
@@ -40,9 +40,8 @@ fn impl_from_with_outermost_macro_implementors_set_bigger_than_innermost_macro_i
 		)
 			.into();
 
-    let toy_finder_implementor = ToyFinderImplementor{ found: [] };
+		let toy_finder_implementor = ToyFinderImplementor { found: [] };
 
 		let _some_struct: SomeStruct = (item_to_impl, item_to_trait, toy_finder_implementor).into();
-
 	});
 }
