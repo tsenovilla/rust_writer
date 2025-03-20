9abf3b7f085c965cb2a4ee1918b43b48a2ce92eb
Tomás Senovilla Polo <tspscgs@gmail.com>
2025-03-20 20:53:28 +0100

    Improve docs

--- rust_writer_procedural/src/lib.rs
+++ rust_writer_procedural/src/lib.rs
@@ -14,7 +14,7 @@ use proc_macro::TokenStream;
 /// precision as if we had used each implementor separately.
 ///
 /// The syntax to use the `#[mutator]` macro is pretty simple:
-/// 
+///
 /// ```rust
 /// use rust_writer::ast::{mutator, implementors::ItemToTrait, mutator::ToMutate};
 /// use syn::visit_mut::VisitMut;
@@ -24,7 +24,7 @@ use proc_macro::TokenStream;
 /// struct NewImplementor;
 /// ```
 ///
-/// From the snippet above we can already assert that the `#[mutator]` macro needs that the traits 
+/// From the snippet above we can already assert that the `#[mutator]` macro needs that the traits
 /// [`ToMutate`](https://docs.rs/rust_writer/latest/rust_writer/ast/mutator/trait.ToMutate.html)
 /// and [`VisitMut`](https://docs.rs/syn/latest/syn/visit_mut/trait.VisitMut.html) need to be
 /// brought into scope.
@@ -40,7 +40,7 @@ use proc_macro::TokenStream;
 /// #[derive(Debug, Clone)]
 /// struct LocalImplementor<T: std::fmt::Debug + Clone>{
 ///   mutated: [bool; 4],
-///   something: T 
+///   something: T
 /// }
 ///
 /// impl<T> VisitMut for LocalImplementor<T> where T: std::fmt::Debug + Clone{
@@ -55,7 +55,25 @@ use proc_macro::TokenStream;
 /// struct NewImplementor;
 /// ```
 ///
-/// Awesome
+/// When the `#[mutator]` macro is called, the struct is expanded an includes a new field for each
+/// implementor named as the implementor type in lowercase. If an implementor is included several
+/// times in the same macro invocation, a suffix is added to the subsequent fields, such as
+/// _1, _2, _3 and so on. For example, the snippet above struct will look like this after
+/// the expansion:
+///
+/// ```ignore
+/// struct NewImplementor<'a, T: std::fmt::Debug + Clone>{
+///   itemtotrait: ItemToTrait<'a>,
+///   itemtotrait_1: ItemToTrait<'a>,
+///   localimplementor: LocalImplementor<T>
+/// }
+/// ```
+///
+/// Awesome, these snippets also show that if an implementor needs a generic, including it in the
+/// macro invocation is enough. Particularly, the generics will be added to the `NewImplementor`
+/// struct, so they'll be available inside it. It's also possible to place.
+///
+/// # The `#[impl_from]` attribute
 ///
 /// # Compatibility with [`#[finder]`](https://docs.rs/rust_writer/latest/rust_writer/ast/attr.finder.html)
 #[proc_macro_attribute]
