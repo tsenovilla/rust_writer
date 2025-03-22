8219e3e09dbe0c26f3e11a7f0811f15b0814bbcd
Tomás Senovilla Polo <tspscgs@gmail.com>
2025-03-22 12:22:53 +0100

    Docs for Mutator and Finder modules

--- rust_writer/src/ast/finder.rs
+++ rust_writer/src/ast/finder.rs
@@ -1,20 +1,111 @@
 // SPDX-License-Identifier: GPL-3.0
 
+//! This module contains the [`Finder`](https://docs.rs/rust_writer/latest/rust_writer/ast/finder/struct.Finder.html)
+//! struct, which is used to search for specific items in an AST in a very targeted way.
+//! The `Finder` struct is completely generic, and thanks to the
+//! [`ToFind`](https://docs.rs/rust_writer/latest/rust_writer/ast/finder/trait.ToFind.html) trait and the
+//! [implementors](https://docs.rs/rust_writer/latest/rust_writer/ast/implementors/index.html) it
+//! may be customized to assert if an element is contained inside an AST.
+//!
+//! # Example
+//! ```rust
+//! use test_builder::TestBuilder;
+//! use rust_writer::ast::{
+//!     finder::{Finder, ToFind},
+//!     implementors::ItemToTrait
+//! };
+//! use syn::{
+//!     visit::Visit,
+//!     parse_quote,
+//!     File
+//! };
+//!
+//! TestBuilder::default()
+//!     .with_trait_ast()
+//!     .execute(|builder| {
+//!         let ast = builder.get_ref_ast_file("trait.rs").expect("This exists; qed;");
+//!
+//!         // Define the ItemFinder implementor. This implementor specifies:
+//!         // 1. The trait we want to search for, e.g., a trait named `MyTrait`.
+//!         // 2. The specific item(s) we expect to find within that trait.
+//!         let item_to_trait: ItemToTrait =
+//!             ("MyTrait", parse_quote! { type Type1: From<String>; }).into();
+//!
+//!         // Create the Finder and load this particular implementor
+//!         let mut finder = Finder::default().to_find(&item_to_trait);
+//!
+//!         // Search the AST for the expected item
+//!         assert!(finder.find(&ast));
+//!     });
+//! ```
+
 #[cfg(test)]
 mod tests;
 
 use std::fmt::Debug;
 use syn::{visit::Visit, File};
 
+/// A placeholder finder which does not perform any specific search.
+/// This is used as the default for a [`Finder`](https://docs.rs/rust_writer/latest/rust_writer/ast/finder/struct.Finder.html).
 #[derive(Debug)]
 pub struct EmptyFinder;
 
+/// The `Finder` struct is used to assert if a concrete element belongs to an AST.
+/// The `Finder` struct is totally generic, so it can be thought of a game console: its purpose is
+/// to determine if an element is contained inside an AST but it doesn't know how to do it until an
+/// [implementor](https://docs.rs/rust_writer/latest/rust_writer/ast/implementors/index.html)
+/// is loaded. This implementor will tell `Finder` how it should behave.
+///
+/// A new `Finder` can be easily created using `Finder::default()`, which returns a
+/// `Finder<'_, EmptyFinder, 1>`. This `Finder` is useless up to this point, but thanks to the
+/// [`ToFind`](https://docs.rs/rust_writer/latest/rust_writer/ast/finder/trait.ToFind.html)
+/// trait, an implementor can be seamlessly loaded to the `Finder`, releasing its whole power.
+///
+/// Once configured, the [`find`](#method.find) method can be called to look inside the AST.
+///
+/// ```rust
+/// use test_builder::TestBuilder;
+/// use rust_writer::ast::{
+///     finder::{Finder, ToFind},
+///     implementors::ItemToTrait
+/// };
+/// use syn::{
+///     visit::Visit,
+///     parse_quote,
+///     File
+/// };
+///
+/// TestBuilder::default()
+///     .with_trait_ast()
+///     .execute(|builder| {
+///         let ast = builder.get_ref_ast_file("trait.rs").expect("This exists; qed;");
+///
+///         // Define the ItemFinder implementor. This implementor specifies:
+///         // 1. The trait we want to search for, e.g., a trait named `MyTrait`.
+///         // 2. The specific item(s) we expect to find within that trait.
+///         let item_to_trait: ItemToTrait =
+///             ("MyTrait", parse_quote! { type Type1: From<String>; }).into();
+///
+///         // Create the Finder and load this particular implementor
+///         let mut finder = Finder::default().to_find(&item_to_trait);
+///
+///         // Search the AST for the expected item
+///         assert!(finder.find(&ast));
+///     });
+/// ```
 #[derive(Debug, Clone)]
 pub struct Finder<'a, T: Debug, const N: usize> {
+	/// A `Finder` can act in different parts of the AST at the same time. This array keeps track
+	/// of which of these searchs succeeded.  
 	pub found: [bool; N],
+	/// Placeholder to load an implementor.
 	pub finder: &'a T,
 }
 
+/// This trait is used to create a new `Finder` variable.
+/// It is typically implemented for `Finder<'_, EmptyFinder, 1>`, enabling the flow:
+/// 1. Create a `Finder<'_, EmptyFinder, 1>` using `Finder::default()`.
+/// 2. Configure it with the desired implementor using the `to_find` method.
 pub trait ToFind<'a, T: Debug, const N: usize> {
 	fn to_find(self, finder: &'a T) -> Finder<'a, T, N>;
 }
@@ -30,6 +121,7 @@ where
 	T: Debug,
 	Finder<'a, T, N>: Visit<'a>,
 {
+	/// Apply all the searches defined by the implementor.
 	pub fn find(&mut self, ast: &'a File) -> bool {
 		self.visit_file(ast);
 		self.found.iter().all(|&x| x)
@@ -40,6 +132,7 @@ impl<T, const N: usize> Finder<'_, T, N>
 where
 	T: Debug,
 {
+	/// Reset the `found` array to all `false` values.
 	pub fn reset(&mut self) {
 		self.found = [false; N];
 	}
--- rust_writer/src/ast/mutator.rs
+++ rust_writer/src/ast/mutator.rs
@@ -1,5 +1,44 @@
 // SPDX-License-Identifier: GPL-3.0
 
+//! This module contains the [`Mutator`](https://docs.rs/rust_writer/latest/rust_writer/ast/mutator/struct.Mutator.html)
+//! struct, which is used to mutate an AST in a very targeted way. The `Mutator` struct is
+//! totally generic, and thanks to the [`ToMutate`](https://docs.rs/rust_writer/latest/rust_writer/ast/mutator/trait.ToMutate.html)
+//! trait and the [implementors](https://docs.rs/rust_writer/latest/rust_writer/ast/implementors/index.html),
+//! it may be customized to mutate the AST in pretty different ways.
+//!
+//! # Example
+//! ```rust
+//! use test_builder::TestBuilder;
+//! use rust_writer::ast::{
+//!   mutator::{Mutator, ToMutate},
+//!   implementors::ItemToTrait
+//! };
+//! use syn::{
+//!   visit_mut::VisitMut,
+//!   parse_quote,
+//!   TraitItem
+//! };
+//!
+//! TestBuilder::default()
+//!  .with_trait_ast()
+//!  .execute(|mut builder|{
+//!   let ast = builder.get_mut_ast_file("trait.rs").expect("This exists; qed;");
+//!
+//!   // Define the ItemToTrait implementor. This implementor means:
+//!   // 1. We're looking inside a trait called `MyTrait`   .
+//!   // 2. We're interested in adding a type called `Type3` with trait bound `From<String>` to
+//!   //    that trait.
+//!   let item_to_trait: ItemToTrait =
+//!    ("MyTrait", parse_quote! {type Type3: From<String>;}).into();
+//!
+//!   // Create the Mutator and load this particular implementor
+//!   let mut mutator = Mutator::default().to_mutate(&item_to_trait);
+//!
+//!   // Mutate the AST
+//!   assert!(mutator.mutate(ast).is_ok());
+//! });
+//! ```
+
 #[cfg(test)]
 mod tests;
 
@@ -7,15 +46,68 @@ use crate::Error;
 use std::fmt::Debug;
 use syn::{visit_mut::VisitMut, File};
 
+/// A placeholder finder which does not perform any specific search.
+/// This is used as the default for a [`Mutator`](https://docs.rs/rust_writer/latest/rust_writer/ast/mutator/struct.Mutator.html) .
 #[derive(Debug, Clone)]
 pub struct EmptyMutator;
 
+/// The `Mutator` struct is used to mutate an AST in very targeted way.
+/// The `Mutator` struct is totally generic, so it can be thought of a game console: its purpose is
+/// to mutate an AST but it doesn't know how to do it until an
+/// [implementor](https://docs.rs/rust_writer/latest/rust_writer/ast/implementors/index.html)
+/// is loaded. This implementor will tell `Mutator` how it should behave.
+///
+/// A new `Mutator` can be easily created using `Mutator::default()`, which returns a
+/// `Mutator<'_, EmptyMutator, 1>`. This `Mutator` is useless up to this point, but thanks to the
+/// [`ToMutate`](https://docs.rs/rust_writer/latest/rust_writer/ast/mutator/trait.ToMutate.html)
+/// trait, an implementor can be seamlessly loaded to the `Mutator`, releasing its whole power.
+///
+/// Once configured, the [`mutate`](#method.mutate) method can be called to mutate the AST.
+///
+/// ```rust
+/// use test_builder::TestBuilder;
+/// use rust_writer::ast::{
+///   mutator::{Mutator, ToMutate},
+///   implementors::ItemToTrait
+/// };
+/// use syn::{
+///   visit_mut::VisitMut,
+///   parse_quote,
+///   TraitItem
+/// };
+///
+/// TestBuilder::default()
+///  .with_trait_ast()
+///  .execute(|mut builder|{
+///   let ast = builder.get_mut_ast_file("trait.rs").expect("This exists; qed;");
+///
+///   // Define the ItemToTrait implementor. This implementor means:
+///   // 1. We're looking inside a trait called `MyTrait`   .
+///   // 2. We're interested in adding a type called `Type3` with trait bound `From<String>` to
+///   //    that trait.
+///   let item_to_trait: ItemToTrait =
+///    ("MyTrait", parse_quote! {type Type3: From<String>;}).into();
+///
+///   // Create the Mutator and load this particular implementor
+///   let mut mutator = Mutator::default().to_mutate(&item_to_trait);
+///
+///   // Mutate the AST
+///   assert!(mutator.mutate(ast).is_ok());
+/// });
+/// ```
 #[derive(Debug, Clone)]
 pub struct Mutator<'a, T: Debug + Clone, const N: usize> {
+	/// A `Mutator` can act in different parts of the AST at the same time. This array keeps track
+	/// of which of these mutations succeeded.  
 	pub mutated: [bool; N],
+	/// Placeholder to load an implementor.
 	pub mutator: &'a T,
 }
 
+/// This trait is used to create new `Mutator` variables. It's typically implemented for
+/// `Mutator<'_, EmptyMutator, 1>`, enabling the flow:
+/// 1. Create a `Mutator<'_, EmptyMutator, 1>` using `Mutator::default()`.
+/// 1. Load the needed implementor using `to_mutate`.
 pub trait ToMutate<'a, T: Debug + Clone, const N: usize> {
 	fn to_mutate(self, mutator: &'a T) -> Mutator<'a, T, N>;
 }
@@ -31,6 +123,7 @@ where
 	T: Debug + Clone,
 	Mutator<'a, T, N>: VisitMut,
 {
+	/// Apply all the mutations defined by the implementor.
 	pub fn mutate(&mut self, ast: &mut File) -> Result<(), Error> {
 		self.visit_file_mut(ast);
 
@@ -46,6 +139,7 @@ impl<T, const N: usize> Mutator<'_, T, N>
 where
 	T: Debug + Clone,
 {
+	/// Reset the `mutated` array to all `false` values.
 	pub fn reset(&mut self) {
 		self.mutated = [false; N];
 	}
