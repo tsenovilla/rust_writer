178dfe12e8e2701f903d5f842a805daa37396c45
Tomás Senovilla Polo <tspscgs@gmail.com>
2025-03-22 18:36:15 +0100

    Preserver type documented

--- rust_writer/src/preserver/types.rs
+++ rust_writer/src/preserver/types.rs
@@ -3,6 +3,40 @@
 #[cfg(test)]
 mod tests;
 
+/// The `Preserver` type specifies which fragments of code should be preserved. It uses a lookup
+/// that identifies the beginning of a line that should be preserved. If such a line doesn't start
+/// a new block (eg, `let something = false;`), that line is the only preserved thing by this type.
+/// If the line opens a new block (eg, `impl MyTrait{`), the whole block is preserved.
+///
+/// It's possible to preserve only a part of a preserved block, while keeping that block itself
+/// preserved by using inner preservers, that is, a `Preserver` contained inside another one (and so
+/// on).
+///
+/// ```no_compile
+/// trait External{
+///   type A: From<String>;
+///
+///   fn inner_function(){
+///     let a = false;
+///     fn super_inner_function(){
+///
+///     }
+///   }
+/// }
+///
+/// // This preserves just super_inner_function.
+/// let preserver = Preserver::new("super_inner_function");
+///
+/// // This preserves inner_function inside the trait, but doesn't preserve the line
+/// // `type A: From<String>`.
+/// let mut preserver = Preserver::new("trait External");
+/// preserver.add_inners(&["fn inner_function"]);
+///
+/// // This preserves super_inner_function inside inner_function inside the trait, but doesn't preserve the lines
+/// // `let a = false;` and `type A: From<String>;`.
+/// let mut preserver = Preserver::new("trait External");
+/// preserver.add_inners(&["fn inner_function", "fn super_inner_function"]);
+/// ```
 #[derive(Debug, Clone, PartialEq)]
 pub struct Preserver {
 	lookup: String,
@@ -10,10 +44,14 @@ pub struct Preserver {
 }
 
 impl Preserver {
+	/// Creates a new preserver using the provided lookup.  
 	pub fn new(lookup: &str) -> Self {
 		Self { lookup: lookup.to_owned(), inner: None }
 	}
 
+	/// Add inner preservers, in order to preserve just an inner block of a preserved block, keeping
+	/// the outer block itself preserved. Inner preserver will be composed sequentially as they are
+	/// defined in the input slice.
 	pub fn add_inners(&mut self, lookups: &[&str]) {
 		let mut current = self;
 		for lookup in lookups {
@@ -22,10 +60,12 @@ impl Preserver {
 		}
 	}
 
+	/// Gets the lookup for a `Preserver`.
 	pub fn lookup(&self) -> &str {
 		&self.lookup
 	}
 
+	/// Gets the outermost inner preserver for a `Preserver`, if any.
 	pub fn get_inner(&self) -> Option<&Preserver> {
 		self.inner.as_deref()
 	}
